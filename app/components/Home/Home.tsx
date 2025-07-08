864fb023f20e52a56aaff72c1ff0b1008166f9f1
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-07-08 21:23:22 +0600

    hotels added

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Destination from "./Destination/Destination";
 import Hero from "./Hero/Hero";
+import Hotel from "./Hotel/Hotel";
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
       <Destination/>
+      <Hotel/>
     </div>
   );
 };
---
+++ app/components/Home/Hotel/Hotel.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import SectionHeading from "../../Helper/SectionHeading";
+import { hotelsData } from "@/app/data/data";
+import HotelCard from "./HotelCard";
+
+const Hotel = () => {
+  return (
+    <div className="pt-20 pb-20">
+      {/* section heading */}
+      <SectionHeading
+        heading="Recommeded Hotels"
+        subHeading="View your stays"
+      />
+      <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-8 items-center mt-16">
+        {/* hotel card */}
+        {hotelsData.map((data)=>{
+            return <div key={data.id}>
+                <HotelCard hotel={data}/>
+            </div>
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Hotel;
